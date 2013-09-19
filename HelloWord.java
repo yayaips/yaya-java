9e836c57d82101e7c8aba2583533c3506049be2f
ysaidji <ysaidji@s29.etu.info-ufr.univ-montp2.fr>
2013-09-19 11:06:06 +0200

    tp1 yaya

---
+++ HelloWord.java
@@ -0,0 +1,15 @@
+public class HelloWord { 
+          
+       public  String  name ;
+       public   HelloWord(String n ){
+		this.name = n ;	
+	}
+	public void  print (){  
+		System.out.println("hello "+this.name);
+         }
+} 
+
+        
+
+ 
+
